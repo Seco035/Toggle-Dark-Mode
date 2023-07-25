157f998c564a4d0d10ca2f0f780d10fb58309a6b
Selcukibar <selcukibarr@icloud.com>
2023-07-25 15:08:17 +0300

    finished

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-
-// javascript
-// document.querySelectorAll("#toggle").forEach(toggle => {
-//     toggle.addEventListener("click", function() {
-//         const checkbox = this.previousElementSibling;
-//         checkbox.checked = !checkbox.checked;
-//     });
-//   });
-// document.getElementById("darkModeCheckbox").addEventListener("click", function() {
-//   const toggle = this.nextElementSibling;
-//   if (this.checked) {
-//     toggle.style.backgroundColor = "#ebebeb";
-//     toggle.innerHTML = '<i class="bi bi-cloud-sun-fill"></i>';
-//       document.body.style.backgroundColor="#ccc";
-//     } else {
-//       toggle.style.backgroundColor = "#242424";
-//       toggle.innerHTML = '<i class="bi bi-cloud-moon-fill"></i>';
-//       document.body.style.backgroundColor="#242424";
-
-//     }
-//   });
-  
 const toggle = document.getElementById("toggle");
 
 document.getElementById("darkModeCheckbox").addEventListener("click", function() {
